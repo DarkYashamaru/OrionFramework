ea934279cf5ccecd9eb4b7ac699dd6243d324426
Yasha <darkyashamaru@gmail.com>
2016-11-01 20:23:48 -0500

    2 on 1 xml, binary SerializationManager finished.

--- Assets/Orion Framework/BaseClases/LoginManager.cs
+++ Assets/Orion Framework/BaseClases/LoginManager.cs
@@ -2,203 +2,215 @@
 using System.IO;
 using System.Text;
 using System.Xml.Serialization;
-using YashaFramework.Serialization;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
 
-namespace YashaFramework
+
+namespace OrionFramework
 {
-	namespace BasicLogin
-	{
-		public static class LoginManager {
-			static SerializationType Serialization;
-			public static string path = "/UserDatabase";
-			public static UserDatabase database;
-			public static UserDatabase Database
+	public static class LoginManager {
+		static SerializationType Serialization;
+		public static string path = "/UserDatabase";
+		static UserDatabase database;
+		public static UserDatabase Database
+		{
+			get
 			{
-				get
-				{
-					if (database == null)
-						LoadOrCreate ();
-					return database;
-				}
+				if (database == null)
+					LoadOrCreate ();
+				return database;
 			}
-
-			static string FileName
+			set 
 			{
-				get 
-				{
-					return path + SerializationManager.GetExtention (Serialization);
-				}
+				database = value;
 			}
+		}
 
-			static void LoadOrCreate ()
+		static string FileName
+		{
+			get 
 			{
+				return path + SerializationManager.GetExtention (Serialization);
+			}
+		}
+
+		static void LoadOrCreate ()
+		{
+			if (database == null) 
+			{
+				if (SerializationManager.FileExist (FileName,Serialization)) {
+					database = SerializationManager.Load<UserDatabase> (FileName, Serialization);
+				}
 				if (database == null) 
 				{
-					if (SerializationManager.FileExist (FileName,Serialization)) {
-
-					} 
-					else 
-					{
-
-					}
+					database = new UserDatabase ();
+					SerializationManager.Save<UserDatabase> (FileName, database, Serialization);
 				}
 			}
-
-			public static void Init (SerializationType serialization)
-			{
-				Serialization = serialization;
-			}
 		}
 
-		[Serializable]
-		public class UserDatabase
+		public static void Save ()
 		{
-			public User[] Users;
+			SerializationManager.Save<UserDatabase> (FileName, database, Serialization);
 		}
 
-		[Serializable]
-		public class User
+		public static void Init (SerializationType serialization)
 		{
-			public string userName = string.Empty;
-			public string id = string.Empty;
-			public UserData[] data;
-		}
-
-		[Serializable]
-		public class UserData
-		{
-			public string name = string.Empty;
-			public string value;
+			Serialization = serialization;
+			LoadOrCreate ();
 		}
 	}
 
-	namespace Serialization
+	[Serializable]
+	public class UserDatabase
 	{
-		public static class SerializationManager
+		public List<User> Users = new List<User>();
+	}
+
+	[Serializable]
+	public class User
+	{
+		public string userName = string.Empty;
+		public string id = string.Empty;
+		public List<UserData> data = new List<UserData>();
+	}
+
+	[Serializable]
+	public class UserData
+	{
+		public string name = string.Empty;
+		public string value;
+	}
+
+	public static class SerializationManager
+	{
+		//Default folder name for serialized assets
+		const string FolderName = "/Resources/";
+		//Default directory for serialized assets (1 folder above current directory plus folder name)
+		static string DirectoryPath
 		{
-			//Default folder name for serialized assets
-			const string FolderName = "/Resources/";
-			//Default directory for serialized assets (1 folder above current directory plus folder name)
-			static string DirectoryPath
+			get
 			{
-				get
-				{
-					return Directory.GetParent(Directory.GetCurrentDirectory())+FolderName;
-				}
+				return Directory.GetParent(Directory.GetCurrentDirectory())+FolderName;
 			}
-			//The custom directory path
-			public static string CustomPath = string.Empty;
-			//The final directory path, the default path is used if custom path is empty
-			public static string FinalPath
+		}
+		//The custom directory path
+		public static string CustomPath = string.Empty;
+		//The final directory path, the default path is used if custom path is empty
+		public static string FinalPath
+		{
+			get
 			{
-				get
-				{
-					return !string.IsNullOrEmpty (CustomPath) ? CustomPath : DirectoryPath;
-				}
-			}
-
-			/// <summary>
-			/// Gets the file extention depending on SerializationType
-			/// </summary>
-			/// <returns>The extention.</returns>
-			/// <param name="serialization">Serialization Type.</param>
-			public static string GetExtention (SerializationType serialization)
-			{
-				switch (serialization)
-				{
-				case SerializationType.Binary:
-					return ".dat";
-				case SerializationType.JSON:
-					return ".json";
-				case SerializationType.XML:
-					return ".xml";
-				default:
-					return ".xml";
-				}
-			}
-
-			public static T Load<T> (string fileName, SerializationType serialization)  where T : class
-			{
-				return Load<T> (fileName, serialization, Encoding.UTF8);
-			}
-
-			public static T Load<T> (string fileName, SerializationType serialization, Encoding encoding)  where T : class
-			{
-				Serializer serializer = new Serializer (serialization,typeof(T));
-				FileStream stream = new FileStream(DirectoryPath+fileName, FileMode.Open);
-				StreamReader sr = new StreamReader(stream, encoding);
-				T t = serializer.Deserialize(sr) as T;
-				stream.Close();
-				return t;
-			}
-
-			public static void Save<T> (string fileName, object data, SerializationType serialization) where T : class
-			{
-				Save<T> (fileName, data, serialization, Encoding.UTF8);
-			}
-
-			public static void Save<T> (string fileName, object data, SerializationType serialization, Encoding encoding) where T : class
-			{
-				Directory.CreateDirectory(FinalPath).Create();
-				Serializer serializer = new Serializer (serialization,typeof(T));
-				FileStream stream = new FileStream(DirectoryPath+fileName, FileMode.Create);
-				StreamWriter streamWriter = new StreamWriter(stream, encoding);
-				serializer.Serialize(streamWriter,data);
-				stream.Close();
-			}
-
-			public static bool FileExist (string path, SerializationType serialization)
-			{
-				return File.Exists(path + GetExtention (serialization));
-			}
-
-			public static void CreateDirectory (string path)
-			{
-				Directory.CreateDirectory(path);
+				return !string.IsNullOrEmpty (CustomPath) ? CustomPath : DirectoryPath;
 			}
 		}
 
-		public enum SerializationType {XML,JSON,Binary};
+		/// <summary>
+		/// Gets the file extention depending on SerializationType
+		/// </summary>
+		/// <returns>The extention.</returns>
+		/// <param name="serialization">Serialization Type.</param>
+		public static string GetExtention (SerializationType serialization)
+		{
+			switch (serialization)
+			{
+			case SerializationType.Binary:
+				return ".dat";
+			case SerializationType.JSON:
+				return ".json";
+			case SerializationType.XML:
+				return ".xml";
+			default:
+				return ".xml";
+			}
+		}
+
+		public static T Load<T> (string fileName, SerializationType serialization)  where T : class
+		{
+			return Load<T> (fileName, serialization, Encoding.UTF8);
+		}
+
+		public static T Load<T> (string fileName, SerializationType serialization, Encoding encoding)  where T : class
+		{
+			Serializer serializer = new Serializer (serialization,typeof(T));
+			FileStream stream = new FileStream(DirectoryPath+fileName, FileMode.Open);
+			StreamReader sr = new StreamReader(stream, encoding);
+			T t = serializer.Deserialize(sr) as T;
+			stream.Close();
+			return t;
+		}
+
+		public static void Save<T> (string fileName, object data, SerializationType serialization) where T : class
+		{
+			Save<T> (fileName, data, serialization, Encoding.UTF8);
+		}
+
+		public static void Save<T> (string fileName, object data, SerializationType serialization, Encoding encoding) where T : class
+		{
+			Directory.CreateDirectory(FinalPath).Create();
+			Serializer serializer = new Serializer (serialization,typeof(T));
+			FileStream stream = new FileStream(DirectoryPath+fileName, FileMode.Create);
+			StreamWriter streamWriter = new StreamWriter(stream, encoding);
+			serializer.Serialize(streamWriter,data);
+			stream.Close();
+		}
+
+		public static bool FileExist (string path, SerializationType serialization)
+		{
+			return File.Exists(DirectoryPath+ path + GetExtention (serialization));
+		}
+
+		public static void CreateDirectory (string path)
+		{
+			Directory.CreateDirectory(path);
+		}
 	}
+
+	public enum SerializationType {XML,JSON,Binary};
 
 	public class Serializer
 	{
 		XmlSerializer xmlSerializer;
+		BinaryFormatter binarySerializer;
+		SerializationType CurrentSerialization;
 
 		public Serializer (SerializationType serialization, Type type)
 		{
+			CurrentSerialization = serialization;
 			switch (serialization) 
 			{
 			case SerializationType.XML:
 				xmlSerializer = new XmlSerializer (type);
 				break;
+			case SerializationType.Binary:
+				binarySerializer = new BinaryFormatter ();
+				break;
 			}
-		}
-		SerializationType currentType;
-
-		public void SetType (SerializationType serialization)
-		{
-			currentType = serialization;
-		}
-
-		public void Serialize (TextWriter textWriter, object o)
-		{
-			
 		}
 
 		public void Serialize (StreamWriter streamWriter, object o)
 		{
-
-		}
-
-		public object Deserialize (TextReader textReader)
-		{
-			return new object ();
+			switch (CurrentSerialization) 
+			{
+			case SerializationType.XML:
+				xmlSerializer.Serialize (streamWriter, o);
+				break;
+			case SerializationType.Binary:
+				binarySerializer.Serialize (streamWriter.BaseStream, o);
+				break;
+			}
 		}
 
 		public object Deserialize (StreamReader streamReader)
 		{
-			return new object ();
+			switch (CurrentSerialization) 
+			{
+			case SerializationType.XML:
+				return xmlSerializer.Deserialize (streamReader);
+			case SerializationType.Binary:
+				return binarySerializer.Deserialize (streamReader.BaseStream);
+			}
+			return null;
 		}
 	}
 }
---
+++ Assets/Orion Framework/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using OrionFramework;
+
+public class TestScript : MonoBehaviour {
+
+	[ContextMenu("Run code")]
+	public void Code1 ()
+	{
+		LoginManager.Init (SerializationType.XML);
+		Debug.Log (LoginManager.Database.Users.Count);
+		UserDatabase ud = LoginManager.Database;
+		ud.Users = new System.Collections.Generic.List<User> ();
+		User u = new User ();
+		ud.Users.Add (u);
+		ud.Users.Add (u);
+		ud.Users.Add (u);
+		LoginManager.Save ();
+	}
+}
